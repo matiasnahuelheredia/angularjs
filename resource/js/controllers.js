0e4aed6cb59467367576813e173715ef4b1cf508
Matias Nahuel Heredia <matias@informaticos.com>
2015-06-29 21:31:59 -0300

    practica final 2/4

--- resource/js/controllers.js
+++ resource/js/controllers.js
@@ -1,4 +1,6 @@
 angular.module("FinalApp")
-.controller("MainController",function($scope){
-
+.controller("MainController",function($scope,$resource){
+	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id",{id:"@id"});
+	$scope.posts = Post.query();
+        console.log($scope.posts);
 });
